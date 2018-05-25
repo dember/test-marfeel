c3bcfa73928a51c067750f00540073f40751c779
William <willie.friede@gmail.com>
2018-05-25 22:52:01 +0200

    Add doctrine-migrations-bundle

--- httpdocs/app/AppKernel.php
+++ httpdocs/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
